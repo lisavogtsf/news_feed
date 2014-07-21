c4e096cb541d45fe5257ff0c17ee21fee43ac147
Lisa Vogt <lisavogt.sf@gmail.com>
2014-07-21 16:48:15 -0700

    tested article routes as well, get only

--- app.js
+++ app.js
@@ -21,4 +21,25 @@ app.get('/contact', function(req, res){
   res.send("contact");
 });
 
+app.get('/articles', function(req, res){
+  // res.render("views/articles/");
+  res.send("article summaries");
+});
+
+app.get('/articles/new', function(req, res){
+  // res.render("views/articles/new");
+  res.send("form to submit new articles");
+});
+
+// app.post('/articles', function(req, res){
+//   // res.render("views/articles");
+//   res.send("save an article");
+// });
+
+app.get('/articles/:id', function(req, res){
+  // res.render("views/articles/:id");
+  res.send("show article with given id");
+});
+
+
 app.listen(3000);
