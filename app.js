30b23b67828d43be26ad095dedd86fb6baaff2f2
Lisa Vogt <lisavogt.sf@gmail.com>
2014-07-22 11:58:02 -0700

    got article summaries working

--- app.js
+++ app.js
@@ -9,6 +9,23 @@ var articleDB = [];
 // folder/page name is articles plural
 // object name in for is article singular
 
+ articleDB = [{
+  title: "Extra! Extra!",
+  author: "Lois Lane",
+  text: "Lorem ipsum blah blah blah."
+  },
+    {
+  title: "Breaking news",
+  author: "Lana Lane",
+  text: "Retro Lorem ipsum blah blah blah."
+  },
+   {
+  title: "Local News Update",
+  author: "Clark Kent",
+  text: "Boring Lorem ipsum blah blah blah."
+  }];
+
+
 // select view engine, ejs
 app.set('view engine', 'ejs');
 
@@ -28,21 +45,27 @@ app.get('/contact', function(req, res){
 
 // dynamic articles pages -- right term?
 app.get('/articles', function(req, res){
-  res.render("articles/summaries");
+  res.render("articles/summaries", {key: articleDB});
 });
 
 app.get('/articles/new', function(req, res){
   res.render("articles/new");
 });
 
+// use sample from class
 app.post('/articles', function(req, res){
   // use articleDB to access array of articles
-  res.send("save an article");
+  // res.send("save an article");
+  console.log(req.body.story);
+  articleDB.push(req.body.story);
+  console.log(articleDB);
+  res.redirect('/articles');  
 });
 
 // so far no successfully saved articles to show
 app.get('/articles/:id', function(req, res){
-  res.render("articles/show", {article: article});
+  res.render("articles/show", {articleDB: article});
+  var id = req.params.id;
 });
 
 
